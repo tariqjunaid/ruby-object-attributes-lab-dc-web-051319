35af34d1cc8e868ab217ac9bfeda486dd5caae4c
tariqjunaid <junaidtariq_91@yahoo.com>
2019-05-20 01:38:58 -0400

    Updated

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,28 @@
 class Dog
-  def name=(dog_name)
-    @name = dog_name
-  end
-  
-  def name
-    @name
-  end
-  
-  def breed=(dog_breed)
-    @breed = dog_breed
-  end
-  
-  def breed
-    @breed
+
+  attr_accessor :name, :breed
+
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
   end
 end
+  # def name=(dog_name)
+  #   @name = dog_name
+  # end
+  
+  # def name
+  #   @name
+  # end
+  
+  # def breed=(dog_breed)
+  #   @breed = dog_breed
+  # end
+  
+  # def breed
+  #   @breed
+  # end
+
+jackie = Dog.new("Jackie", "German Shepherd")
+puts jackie.name
+puts jackie.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,26 @@
 class Person
-  def name=(person_name)
-    @name = person_name
+  attr_accessor :job
+  def initialize(full_name)
+    @first_name, @last_name = full_name.split
   end
-  
+ 
+  def name=(full_name)
+    @first_name, @last_name = full_name.split
+  end
+ 
   def name
-    @name
-  end
-  
-  def job=(person_job)
-    @job = person_job
-  end
-  
-  def job
-    @job
+    "#{@first_name} #{@last_name}".strip
   end
 end
+  
+  # def job=(person_job)
+  #   @job = person_job
+  # end
+  
+  # def job
+  #   @job
+  # end
+
+khan = Person.new("Junaid Tariq")
+puts khan.name
+puts khan.job = "developer"
